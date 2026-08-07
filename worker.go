719e9d222b25d54f6135e2bb6e95748370d30629
Maddie Schipper <me@maddiesch.com>
2026-08-06 20:18:40 -0600

    add value worker

--- worker.go
+++ worker.go
@@ -1,7 +1,27 @@
 package raptor
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+)
 
 type Worker interface {
 	Execute(context.Context, *Job) error
+}
+
+func ValueWorkerFunc[T any](fn func(context.Context, *Job, T) error) Worker {
+	return &valueWorkerFunc[T]{fn: fn}
+}
+
+type valueWorkerFunc[T any] struct {
+	fn func(context.Context, *Job, T) error
+}
+
+func (w *valueWorkerFunc[T]) Execute(ctx context.Context, job *Job) error {
+	var value T
+	if err := json.Unmarshal(job.Payload, &value); err != nil {
+		return err
+	}
+
+	return w.fn(ctx, job, value)
 }
